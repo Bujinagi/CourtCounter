78c50a4fba85d21888f474b6c3adcf5e24f5045b
Brandon Nguyen <brandonnguyen626@gmail.com>
2016-11-07 10:12:51 -0800

    1 Point button that actually +1
    made the one point button and when you click it, it adds a point to our
    integer

--- app/src/main/java/br/courtcounter/MainActivity.java
+++ app/src/main/java/br/courtcounter/MainActivity.java
@@ -2,12 +2,27 @@ package br.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
+    private TextView counts;
+    public int a;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counts = (TextView)findViewById(R.id.count);
+        counts.setText(String.valueOf(a));
     }
+
+    public void clickFunction1(View view){
+        counts.setText(String.valueOf(a));
+        counts.setText(""+a);
+        a++;
+    }
+
+
 }
