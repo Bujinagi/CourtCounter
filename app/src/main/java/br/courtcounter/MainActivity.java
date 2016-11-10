3ba36c917bfdb3d03da142db3986f0a28a64991e
Brandon Nguyen <brandonnguyen626@gmail.com>
2016-11-10 09:13:50 -0800

    Added a proper reset button
    finished milestone 5

--- app/src/main/java/br/courtcounter/MainActivity.java
+++ app/src/main/java/br/courtcounter/MainActivity.java
@@ -8,8 +8,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private TextView counts;
     private TextView counts2;
-    public int a;
-    public int b;
+    private static int a;
+    private static int b;
 
 
     @Override
@@ -25,15 +25,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickFunction1(View view){
         counts.setText(String.valueOf(a));
-        counts.setText(""+a);
         a++;
+        counts.setText(""+a);
     }
 
     public void clickFunction2(View view){
         counts2.setText(String.valueOf(b));
-        counts2.setText(""+b);
         b++;
+        counts2.setText(""+b);
     }
 
+    public void clickFunction3(View view){
+        counts.setText("0");
+        counts2.setText("0");
+        this.a = 0;
+        this.b = 0;
+    }
 
 }
