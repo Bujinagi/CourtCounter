fa13ce426683859e4274aad880c2243b25bdc36f
Brandon Nguyen <brandonnguyen626@gmail.com>
2016-11-10 08:49:33 -0800

    Made a Second Team

--- app/src/main/java/br/courtcounter/MainActivity.java
+++ app/src/main/java/br/courtcounter/MainActivity.java
@@ -7,7 +7,9 @@ import android.widget.Button;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private TextView counts;
+    private TextView counts2;
     public int a;
+    public int b;
 
 
     @Override
@@ -16,12 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         counts = (TextView)findViewById(R.id.count);
         counts.setText(String.valueOf(a));
+        counts2 = (TextView)findViewById(R.id.count2);
+        counts2.setText(String.valueOf(b));
+
     }
 
     public void clickFunction1(View view){
         counts.setText(String.valueOf(a));
         counts.setText(""+a);
         a++;
+    }
+
+    public void clickFunction2(View view){
+        counts2.setText(String.valueOf(b));
+        counts2.setText(""+b);
+        b++;
     }
 
 
