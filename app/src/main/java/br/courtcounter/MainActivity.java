8d382c3b6cabb926f485bf7d0a12301dc576203c
Brandon Nguyen <brandonnguyen626@gmail.com>
2016-11-18 08:43:23 -0800

    Added sounds to my Court Counter
    finished milestone 7 of mobile dev

--- app/src/main/java/br/courtcounter/MainActivity.java
+++ app/src/main/java/br/courtcounter/MainActivity.java
@@ -1,19 +1,23 @@
 package br.courtcounter;
 
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
     private TextView counts;
     private TextView counts2;
     private static int a;
     private static int b;
+    private MediaPlayer sound1;
+    private MediaPlayer sound2;
+    private MediaPlayer sound3;
     AlertDialog gameOverAlert;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +28,21 @@ public class MainActivity extends AppCompatActivity {
         counts2 = (TextView)findViewById(R.id.count2);
         counts2.setText(String.valueOf(b));
         gameOverAlert= new AlertDialog.Builder(MainActivity.this).create();
+        sound1 = MediaPlayer.create(this , R.raw.airhorn);
+        sound2 = MediaPlayer.create(this , R.raw.hitmarker);
+        sound3 = MediaPlayer.create(this , R.raw.hitmarkerz);
 
     }
 
     public void clickFunction1(View view){
+        sound2.start();
         counts.setText(String.valueOf(a));
         a++;
         counts.setText(""+a);
     }
 
     public void clickFunction2(View view){
+        sound3.start();
         counts2.setText(String.valueOf(b));
         b++;
         counts2.setText(""+b);
@@ -43,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
 
         gameOverAlert.setTitle("Alert Text");
         gameOverAlert.setMessage("Message Text");
+
+        sound1.start();
 
         gameOverAlert.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
                 new DialogInterface.OnClickListener() {
